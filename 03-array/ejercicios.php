7c749a920a1a83e0a761d220763c6c3762fd4e79
Agustin <agustind1212@gmail.com>
2024-10-04 14:37:45 +0200

    asdf

--- 03-array/ejercicios.php
+++ 03-array/ejercicios.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css">
+    <?php
+        error_reporting( E_ALL );
+        ini_set( "display_errors", 1 );    
+    ?>
+</head>
+<body>
+    
 <!--EJERCICIO 1
 
     Desarrollo web en entorno servidor => Alejandra
@@ -8,17 +22,45 @@
     Ingles => Virginia
     
     MOSTRARLO TODO EN UNA TABLA-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="estilos.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-    
-</body>
+
+<!--Resuelto con la profe-->
+
+<?php
+
+    $asignaturas = [
+        "Desarrollo web en entorno servidor" => "Alejandra",
+        "Desarrollo  Web en entorno cliente" => "José Miguel",
+        "Diseño de interfaces web" => "José Miguel",
+        "Despliegue de aplicaciones" => "Jaime",
+        "Empresa e iniciativa emprendedora" => "Andrea",
+        "Ingles" => "Virginia"
+    ];
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Asignatura</th>
+            <th>Profesor</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        ksort($asignaturas); //para ordenarlo (ver web php las diferentes opciones de ordenamiento de arrays)
+        foreach($asignaturas as $asignatura => $profesor) {//IMPORTANTE EN LOS NOMBRES, EL ARRAY EN PLURAL Y LUEGO EN SINGULAR PARA CADA UNO
+            echo "<tr>";
+            echo "<td>$asignatura</td>";
+            echo "<td>$profesor</td>";
+            echo "</tr>";
+
+        }
+        ?>
+    </tbody>
+</table>
+
+
+<!--RESUELTO POOR MI-->
+
 </html>
     <table>
         <caption>Asignaturas</caption>
@@ -56,6 +98,7 @@
 
         <tbody>
     </table>
+    <br><br><br>
 
 <!--EJERCICIO 2
 
@@ -69,6 +112,53 @@
     -COLUMNA 2: NOTA
     -COLUMNA 3: SI NOTA < 5 SUSPENSO, SINO APROBADO
 -->
+
+    <!--RESUELTO POR LA PROFESORA0-->
+
+    <?php
+        $estudiantes = [
+            "Francisco" => 3,
+            "DAniel" => 5,
+            "Aurora" => 10,
+            "Ismael" => 0,
+            "Luis" => 7,
+            "Samuel" => 3,
+            "Juanjo" => 2,
+            "Vicente" => 1,
+        ];
+    ?>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Estudiante</th>
+                <th>Nota</th>
+                <th>Calificación</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            //if($nota < 5) echo "<tr class='Suspenso'>";
+            //else echo "<tr class='Aprobado'>";
+
+            foreach($estudiantes as $estudiante => $nota) { ?> 
+                <tr class="<?php if($nota < 5) echo "suspenso"; else echo "aprobado"; ?>">
+                
+                    <td><?php echo $estudiante ?></td>
+                    <td><?php echo $nota ?></td>
+                    <td>
+                        <?php
+                            if($nota < 5){
+                                echo "Suspenso";
+                            }else{
+                                echo"Aprobado";
+                            }
+                        ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 
 <?php
 $alumno;
@@ -186,4 +276,20 @@ $condicion;
         </tr>
     </tbody>
 </table>
+<?php
+/**
+ * Insertar dos nuesvos estudiantes, con notas aleatorias entre 0 y 10
+ * 
+ * Borrar une studiante por clave
+ * 
+ * Mostrar en una nueva tabla todo ordenado por los nombres en orden alfabeticamente inverso
+ * 
+ * Mostrar en una nueva tabla todo ordenado por la nota de 10 a 0 (orden inverso)
+ * 
+ */
+?>
+
+
+</body>
+</html>
 
